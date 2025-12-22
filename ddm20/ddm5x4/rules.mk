2d9ca788fcf90c745e1599d670020b088358b192
Daniel Durán <33481216+dandmol@users.noreply.github.com>
2025-12-22 21:58:09 +0100

    Delete ddm20/ddm5x4 directory

--- ddm20/ddm5x4/rules.mk
+++
@@ -1,6 +0,0 @@
-# Para no heredar el “Pro Micro RP2040” por defecto
-BOARD = GENERIC_RP_RP2040
-
-# Para sacar consola de depuración (muy útil para ver matriz)
-CONSOLE_ENABLE = yes
-COMMAND_ENABLE = yes
