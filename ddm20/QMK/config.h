60f91097ad7ff485504afaa7544188898e17c55d
Daniel Durán <33481216+dandmol@users.noreply.github.com>
2025-12-28 13:36:09 +0100

    Add files via upload

--- ddm20/QMK/config.h
+++ ddm20/QMK/config.h
@@ -1,6 +1,9 @@
 #pragma once
 
-// Configuración específica del teclado ddm5x4
+// Configuración específica del teclado ddm20
+
+// LED indicador de capa en GP1
+#define LED_LAYER_PIN GP1
 
 #ifdef COMBO_ENABLE
     // Tiempo máximo (en ms) entre pulsaciones para que cuente como combo.
